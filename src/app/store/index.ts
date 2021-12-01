aaf57ca27bf505949a7d8ea0dbc0da1a66c80483
Gergely Padányi-Gulyás <work@gpadanyig.com>
2021-12-01 21:39:01 +0100

    add metareducer to log state and actions on the console

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,4 +1,4 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
 
 import { environment } from '../../environments/environment';
 import * as fromUser from './user/user.reducer';
@@ -11,6 +11,15 @@ export const reducers: ActionReducerMap<AppState> = {
   user: fromUser.reducer,
 };
 
+// console.log all actions
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return function (state, action) {
+    console.log('%c state ', 'color: blue', state);
+    console.log('%c action ', 'color: magenta', action);
+    return reducer(state, action);
+  };
+}
+
 export const metaReducers: MetaReducer<AppState>[] = !environment.production
-  ? []
+  ? [debug]
   : [];
