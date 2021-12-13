cf7a39a4802754726a21e1b64133fbd438bb2cc0
Gergely Padányi-Gulyás <work@gpadanyig.com>
2021-12-13 16:55:50 +0100

    add test to HomeService

--- src/app/pages/home/home.service.spec.ts
+++ src/app/pages/home/home.service.spec.ts
@@ -1,28 +1,52 @@
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
 import { StoreModule } from '@ngrx/store'
+import { of } from 'rxjs'
 
 import { reducers } from '../../store/app.state'
 import { HomeApiService } from './api/home.api.service'
 import { HomeService } from './home.service'
+import { Joke } from './models/joke.interface'
 import { reducers as homeReducers } from './state/home.state'
 
 describe('HomeService', () => {
   let service: HomeService
+  let homeApiServiceSpy: jasmine.SpyObj<HomeApiService>
 
   beforeEach(() => {
+    const spy = jasmine.createSpyObj('HomeApiService', ['fetchJoke$'])
     TestBed.configureTestingModule({
       imports: [
-        HttpClientModule,
+        HttpClientTestingModule,
         StoreModule.forRoot(reducers),
         StoreModule.forFeature('home', homeReducers)
       ],
-      providers: [HomeApiService]
+      providers: [{ provide: HomeApiService, useValue: spy }]
     })
     service = TestBed.inject(HomeService)
+    homeApiServiceSpy = TestBed.inject(
+      HomeApiService
+    ) as jasmine.SpyObj<HomeApiService>
   })
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('should call api fetchJoke', (done: DoneFn) => {
+    const expectedJoke: Joke = {
+      id: '123213',
+      joke: 'It was raining cats and dogs the other day. I almost stepped in a poodle.',
+      status: 200
+    }
+    homeApiServiceSpy.fetchJoke$.and.returnValue(of(expectedJoke))
+    service.fetchJoke$().subscribe({
+      next: (joke) => {
+        expect(homeApiServiceSpy.fetchJoke$).toHaveBeenCalledTimes(1)
+        expect(joke).toEqual(expectedJoke)
+        done()
+      },
+      error: () => done.fail
+    })
   })
 })
