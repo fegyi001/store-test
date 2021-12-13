18246017cd60d9d238144ca6648afedb28e938d4
Gergely Padányi-Gulyás <work@gpadanyig.com>
2021-12-13 17:10:25 +0100

    add test to JokeComponent

--- src/app/pages/home/components/joke/joke.component.spec.ts
+++ src/app/pages/home/components/joke/joke.component.spec.ts
@@ -1,10 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
 
 import { JokeComponent } from './joke.component'
 
 describe('JokeComponent', () => {
   let component: JokeComponent
   let fixture: ComponentFixture<JokeComponent>
+
+  const defaultJoke = {
+    id: 'qrOfN7w5ojb',
+    joke: 'A cannibal is someone who is fed up with people.',
+    status: 200
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,15 +22,17 @@ describe('JokeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(JokeComponent)
     component = fixture.componentInstance
-    component.joke = {
-      id: 'qrOfN7w5ojb',
-      joke: 'A cannibal is someone who is fed up with people.',
-      status: 200
-    }
+    component.joke = defaultJoke
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should show joke', () => {
+    const element = fixture.debugElement.query(By.css('.joke'))
+    expect(element).toBeTruthy()
+    expect(element.nativeElement.textContent).toEqual(defaultJoke.joke)
   })
 })
